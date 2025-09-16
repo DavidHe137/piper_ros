b412bd66a3e0e2e5671f4a0092593ebea9644031
Soobum Kim <skim743@gatech.edu>
2025-09-16 16:44:55 -0400

    Updated docker files.

--- docker/docker_run_dev.sh
+++ docker/docker_run_dev.sh
@@ -6,9 +6,10 @@ docker run -it \
            --net=host \
            --runtime=nvidia \
            --gpus all \
-           --name piper_sim_mujoco \
+           --name piper_env \
            -e NVIDIA_DRIVER_CAPABILITIES=all \
            -e DISPLAY=$DISPLAY \
 	       -v /tmp/.X11-unix/:/tmp/.X11-unix/:rw \
            -v "$(pwd)/../data:/app/data" \
-           piper_sim:mujoco
+           -v /dev:/dev \
+           piper_ros_sim:humble
