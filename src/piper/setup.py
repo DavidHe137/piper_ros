8a49d6e255839aff442053e8cab8300c8cd12052
Soobum Kim <skim743@gatech.edu>
2025-09-30 17:03:46 -0400

    Added ROS joint state broadcaster

--- src/piper/setup.py
+++ src/piper/setup.py
@@ -30,6 +30,7 @@ setup(
             'piper_single_ctrl = piper.piper_ctrl_single_node:main',
             'piper_ms_ctrl = piper.piper_start_ms_node:main',
             'piper_read_master = piper.piper_read_master_node:main',
+            'piper_broadcast_master = piper.piper_broadcast_master:main',
         ],
     },
 )
