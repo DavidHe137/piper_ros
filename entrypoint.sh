b2d76056492ea9fe5f9ad9603285a1f66fadbf13
David He <davidhe@gatech.edu>
2026-04-02 17:11:41 -0400

    Make build outputs deletable.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ set -e
 source /opt/ros/humble/setup.bash
 export PYTHONPATH=/piper_sdk:$PYTHONPATH
 cd /piper_ros && rm -rf build install log && colcon build
+chmod -R 777 /piper_ros/build /piper_ros/install /piper_ros/log
 cd -
 source /piper_ros/install/setup.bash
 cd -
